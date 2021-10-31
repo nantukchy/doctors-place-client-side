c5edbb0bf236ed72eff20589f5341bb6627c464f
Rahul Chowdhury <nantukchy@gmail.com>
2021-10-31 21:07:27 +0600

    final

--- src/components/Pages/allServices/AllServices.js
+++ src/components/Pages/allServices/AllServices.js
@@ -9,9 +9,9 @@ const AllServices = ({ service }) => {
         
         <div className="service bg-warning pb-3">
             <img className="img-fluid" src={img} alt="" />
-        <h2>{name}</h2>
-        <h5>Price range start from $ {price}</h5>
-            <p className="px-3 ">{description.slice(0, 100)}</p>
+        <h2 className="ms-1">{name}</h2>
+        <h5 className="text-end me-1">Price range start from $ {price}</h5>
+            <p className="px-3 ">{description.slice(0, 200)}...</p>
             <Link to={`/wishlist/${_id}`}>
                 <Button className="btn btn-danger">Add to WishList</Button>
             </Link>
